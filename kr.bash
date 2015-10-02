5122cd4bb08b1d1d343f93ca1ea4c3994f052c24
Alvin Estrada <adawolfs@gmail.com>
2015-10-01 20:30:13 -0600

    add midi

--- kr.bash
+++ kr.bash
@@ -1,12 +1,23 @@
 #!/bin/bash
+{
+	timidity ./kirby.mid
+} &
 while(true)
 do
+	printf "\033c"
+	./1.bash
 	./1.bash
 	sleep 0.2
+	printf "\033c"
 	./2.bash
-	sleep 0.2	
+	./2.bash
+	sleep 0.2
+	printf "\033c"
+	./3.bash
 	./3.bash
 	sleep 0.2
+	printf "\033c"
+	./2.bash
 	./2.bash
 	sleep 0.2
 done
